d7ee50063316ea817a1ee59f6b3633a176e2ae9b
LIka1599 <99115297+LIka1599@users.noreply.github.com>
2022-03-06 23:53:33 +0700

    Update index.php

--- form/index.php
+++ form/index.php
@@ -26,7 +26,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="Simple-Alert-Dialog-Box-Plugin-For-jQuery-al-js/src/al.css">
     <title>User</title>
 </head>
